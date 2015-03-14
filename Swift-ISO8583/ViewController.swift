c114db8bf72ed635a7734e24e651c86fde4bf822
Jorge Tapia <jtapia@mindshake.net>
2015-03-14 18:18:44 -0400

    Adds project files.

---
+++ Swift-ISO8583/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-ISO8583
+//
+//  Created by Jorge Tapia on 3/14/15.
+//  Copyright (c) 2015 Jorge Tapia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
