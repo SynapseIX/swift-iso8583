7ae4d6d00563f83e42761a41804548264e4d38ab
Jorge Tapia <jtapia@mindshake.net>
2015-03-15 15:36:10 -0400

    Adds basic class files and ISO-8583 config files.

---
+++ Swift-ISO8583/ISO-8583 Lib/ISODataElement.swift
@@ -0,0 +1,9 @@
+//
+//  ISODataElement.swift
+//  Swift-ISO8583
+//
+//  Created by Jorge Tapia on 3/15/15.
+//  Copyright (c) 2015 Jorge Tapia. All rights reserved.
+//
+
+import Foundation
---
+++ Swift-ISO8583/ISO-8583 Lib/ISOHelper.swift
@@ -0,0 +1,9 @@
+//
+//  ISOHelper.swift
+//  Swift-ISO8583
+//
+//  Created by Jorge Tapia on 3/15/15.
+//  Copyright (c) 2015 Jorge Tapia. All rights reserved.
+//
+
+import Foundation
