c92343909cd48390563b4114213898ee7b2f7815
Jorge Tapia <jtapia@mindshake.net>
2015-03-17 20:03:49 -0400

    Implements class until hexToBinaryAsString method.

--- Swift-ISO8583/ISO-8583 Lib/ISOHelper.swift
+++ Swift-ISO8583/ISO-8583 Lib/ISOHelper.swift
@@ -9,16 +9,59 @@
 import Foundation
 
 class ISOHelper {
-    class func stringToArray(string: String) -> [String] {
-        return []
+    class func stringToArray(string: String?) -> [String]? {
+        if string == nil {
+            return nil
+        }
+        
+        var chars = [String]()
+        
+        for char in string! {
+            chars.append("\(char)")
+        }
+        
+        return chars
     }
     
-    class func arrayToString(array: [String]) -> String {
-        return String()
+    class func arrayToString(array: [String]?) -> String? {
+        if array == nil {
+            return nil
+        }
+        
+        var string = String()
+        
+        for char in array! {
+            string.append(Character(char))
+        }
+        
+        return string
     }
     
-    class func hexToBinaryAsString(hexString: String) -> String {
-        return String()
+    class func hexToBinaryAsString(hexString: String?) -> String? {
+        if hexString == nil {
+            return nil
+        }
+        
+        // Validate if it's a hexadecimal number
+        var regExPattern = "[0-9A-F]"
+        var regEx = NSRegularExpression(pattern: regExPattern, options: nil, error: nil)
+        var regExMatches = regEx?.numberOfMatchesInString(hexString!, options: nil, range: NSMakeRange(0, countElements(hexString!)))
+        
+        if regExMatches! != countElements(hexString!) {
+            println("Parameter \(hexString) is an invalid hexadecimal number.")
+            return nil;
+        }
+        
+        let conversionTable = ["0": "0000", "1": "0001", "2": "0010", "3": "0011", "4": "0100", "5": "0101", "6": "0110", "7": "0111", "8": "1000", "9": "1001", "A": "1010", "B": "1011", "C": "1100", "D": "1101", "E": "1110", "F": "1111"]
+        
+        let hexArray = stringToArray(hexString)!
+        var result = String()
+        
+        for hexNumber in hexArray {
+            result += conversionTable[hexNumber]!
+        }
+        
+        return result
     }
     
     class func binaryToHexAsString(binaryString: String) -> String {
